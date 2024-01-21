884f319b49ee94f659690624a6df5e3089f3044f
Wande <wanderlustking11@gmail.com>
2024-01-21 00:17:16 -0500

    Update Day 1; daily exercises and project

--- Day001/Day1.py
+++ Day001/Day1.py
@@ -9,12 +9,12 @@ This is my attempt to do this on my own."""
 
 
 
-# Topics for Day 1
+########## Topics for Day 1 ##########
 # print()
 # Strings are anyting in between quotes " "
 # Syntax highlighting, how our Code Editors give us clues as to what code is
 
-# Here is the Day 1 Printing exercise:
+######### Here is the Day 1 Printing exercise: ############
 
 # Print the three lines correctly, and submit to test if it's correct.
 # Below is the correct answer.
@@ -24,7 +24,7 @@ print("The function is declared like this:")
 print("print('what to print')")
 
 
-# More topics:
+### More topics:
 # printing mutliple lines
 
 
@@ -43,7 +43,7 @@ print('May ' + 'the ' + 'force ' + 'be ' + 'with ' + 'you' + '!')
 
 # debugging
 
-# We have a Day 1 Debugging Practice exercise
+############ We have a Day 1 Debugging Practice exercise #############
 # This is the desired output:
 """
 Day 1 - String Manipulation
@@ -65,7 +65,7 @@ print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 
 
-# Here's the Day 1 Practice:
+######## Here's the Day 1 Input Function: #########
 # Write some code that will print you multiplying num1 with num2.
 # num1 = int(input())
 # num2 = int(input())
@@ -74,3 +74,32 @@ print("New lines can be created with a backslash and n.")
 # Now write some code that will print the number of charcters in a name from a given input:
 name = input("What's your name? ")
 print(len(name))
+
+
+# Variables
+
+########### Day Variables  #############
+# Switch the values of input a and b so they print opposite of what the user input was.
+# a = input("Enter a number for A: ")
+# b = input("Enter a number for B: ")
+# c = a
+# a = b
+# b = c
+# print("a: " + a)
+# print("b: " + b)
+
+
+###################################################################
+
+##### The Final Project #####
+
+#1. Create a greeting for your program
+print("Welcome to band name generator.")
+#2. Ask the user for the city that they grew up in.
+city = input("Which city did you grow up in?")
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of your pet?")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + " " + pet)
+# 5. Make sure the input cursor shows on a new line, see example at:
+# https://band-name-generator-end.apbrewery.repl.run/
